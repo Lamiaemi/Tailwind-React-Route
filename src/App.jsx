3e75b06cbc4a9ccd998fd5bd70a450ebe93528d3
MDParvezsarkar <mdparvezsarkar10@gmail.com>
2025-04-13 17:10:07 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1 className='text-4xl '>Vite + React</h1>
+      <p className='docs'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium maiores quos voluptatem, vel perferendis omnis enim expedita. Voluptatibus, deserunt exercitationem sapiente culpa quae, modi neque molestias soluta aut, labore temporibus.c</p>
+      
+    </>
+  )
+}
+
+export default App
